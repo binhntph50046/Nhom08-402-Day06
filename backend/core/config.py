7a2af6a6f340f3012a783471b97b1b161d31ae13
nguyenhoangviethung <nguyenhoangviethung@gmail.com>
2026-04-09 17:52:28 +0700

    chore:

--- backend/core/config.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-import os
-from dotenv import load_dotenv
-load_dotenv()
-CONFIG = {
-    "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-    "LLM": ChatOpenAI(model = os.getenv("MODEL"), temperature=0, api_key=os.getenv("OPENAI_API_KEY")) 
-}
-     
