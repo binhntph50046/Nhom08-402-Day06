0e2f750bb0f0e0e332396d2024d2d56e3c4590b2
binhntph50046 <binhntph50046>
2026-04-09 21:33:18 +0700

    Clean up main: keep only Nhom08-402-Day06

--- src/functions/configs.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: "https://joyfilm-server.netlify.app/.netlify/functions/actions",
-});
-
-export default axiosInstance;
